c804230f8ec2ecde7365bf07ba56fdcd8e3c4b3b
Austin Oblow <austinoblow97@gmail.com>
2023-11-15 18:47:06 -0600

    when the app starts the plants render on the page

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,16 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plant }) {
+
+  const newCards = plant.map((plant) => {
+    return (<PlantCard key={plant.id} plant={plant} />)
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {newCards}
+    </ul>
   );
 }
 
